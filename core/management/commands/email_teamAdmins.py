f8b5f6eb8d78aed9a54335c92a5ddea7070b9ecf
dvadgama <dhaval.it@gmail.com>
2020-05-22 09:16:49 +0100

    fix: multiple emails going to team admin with multiple email  - now it sends a single consolidate email to team admin, even if he/she is team admin for multiple teams in githubscan

--- core/management/commands/email_teamAdmins.py
+++ core/management/commands/email_teamAdmins.py
@@ -12,21 +12,46 @@ class Command(BaseCommand):
         try:
             dbData = Data()
             admin_emails = dbData.getTeamAdminEmails()
+
+           
             for email in admin_emails:
                 teams = dbData.getTeamsByAdminEmail(admin_email=email['admin_email'])
 
-            for team in teams:
-                if (team.admin_email):
 
-                    admin_emails = team.admin_email.split(',')
-                    report = Report()
+                report_data = {}
+                for team in teams:
+                    if (team.admin_email):
+                        admin_emails = team.admin_email.split(',')
+                        report = Report()
+                        team_report = report.getTeamReport(team=team.name)
+        
+                        report_data = self.merge_reports(report_data,team_report)                     
+                        
+            
+                if(report_data):
                     process_report = ReportData()
-                    data = process_report.format(
-                        raw_report=report.getTeamReport(team=team.name))
-
-                    if (data):
-                        Email(admin_emails, data)
+                    data = process_report.format(raw_report=report_data)
+                
+                    Email(admin_emails, data)
 
         except Exception as e:
             print("Team Email Send Error:{}".format(e))
             traceback.print_exc()
+            
+            
+    def merge_reports(self,report_data={},team_report={}):
+        report_repos = set(report_data.keys())
+        team_report_repos = set(team_report.keys())
+
+        existing_repos = report_repos.intersection(team_report_repos)
+        new_repos = team_report_repos.difference(report_repos)
+
+
+        for repo in new_repos:
+            report_data[repo] = team_report[repo]
+
+        for repo in existing_repos:
+            report_data[repo]['teams'] +=  team_report[repo]['teams']
+
+        return report_data
+
